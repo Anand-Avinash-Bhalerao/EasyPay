be3d0bda2d49517ddca0b35256da36c5564cd906
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2023-11-26 01:09:53 +0530

    CommonScaffold composable added.

---
+++ app/src/main/java/com/billion_dollor_company/easypay/ui/components/CommonScaffold.kt
@@ -0,0 +1,64 @@
+package com.billion_dollor_company.easypay.ui.components
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.billion_dollor_company.easypay.ui.home.CompanyLogo
+import androidx.compose.material3.Icon
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonScaffold(
+    hasFAB: Boolean = false,
+    iconFAB: ImageVector? = null,
+    onFABClick : () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        floatingActionButton = {
+            if (hasFAB) {
+                FloatingActionButton(
+                    onClick = {
+                        onFABClick()
+                    }
+                ) {
+                    Icon(
+                        imageVector = iconFAB!!,
+                        contentDescription = "FAB Icon"
+                    )
+                }
+            }
+        }
+    ) { paddingValue ->
+        val scrollState = rememberScrollState()
+        Surface(
+            modifier = Modifier
+                .padding(paddingValue)
+                .fillMaxSize()
+                .verticalScroll(state = scrollState)
+        ) {
+            Column( // or whatever your parent composable is
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 12.dp)
+            ) {
+                content()
+            }
+        }
+    }
+}
--- app/src/main/java/com/billion_dollor_company/easypay/ui/home/HomeScreen.kt
+++ app/src/main/java/com/billion_dollor_company/easypay/ui/home/HomeScreen.kt
@@ -1,61 +1,53 @@
 package com.billion_dollor_company.easypay.ui.home
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Money
+import androidx.compose.material.icons.filled.PriceChange
 import androidx.compose.material.icons.filled.QrCodeScanner
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import com.billion_dollor_company.easypay.ui.components.CommonScaffold
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    val viewModel: HomeViewModel = hiltViewModel()
-    val scrollState = rememberScrollState()
+    CommonScaffold(
+        hasFAB = true,
+        iconFAB = Icons.Default.QrCodeScanner,
+    ) {
+        CompanyLogo()
+    }
+}
 
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.QrCodeScanner,
-                    contentDescription = "Scan QR Code"
-                )
-            }
-        }
-    ) { paddingValue ->
-        Surface(
+@Composable
+fun CompanyLogo() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.PriceChange,
+            contentDescription = "Money Logo",
+            tint = MaterialTheme.colorScheme.primary,
             modifier = Modifier
-                .padding(paddingValue)
-                .fillMaxSize()
-        ) {
-
-        }
+                .size(36.dp)
+        )
+        Text(
+            text = "EasyPay",
+            style = MaterialTheme.typography.displaySmall.copy(
+                fontWeight = FontWeight.ExtraBold
+            ),
+            modifier = Modifier
+                .padding(start = 12.dp)
+        )
     }
 }
