2984c72e2d6cd3ed381ca68337a639edd299378b
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2023-11-26 23:13:04 +0530

    Home Screen UI complete

--- app/src/main/java/com/billion_dollor_company/easypay/ui/home/HomeScreen.kt
+++ app/src/main/java/com/billion_dollor_company/easypay/ui/home/HomeScreen.kt
@@ -1,116 +1,65 @@
 package com.billion_dollor_company.easypay.ui.home
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PriceChange
 import androidx.compose.material.icons.filled.QrCodeScanner
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.billion_dollor_company.easypay.R
 import com.billion_dollor_company.easypay.ui.components.CommonScaffold
+import com.billion_dollor_company.easypay.ui.components.HeightSpacer
+import com.billion_dollor_company.easypay.ui.home.components.CompanyLogo
+import com.billion_dollor_company.easypay.ui.home.components.CreditCard
+import com.billion_dollor_company.easypay.ui.home.components.MoreSection
+import com.billion_dollor_company.easypay.ui.home.components.PaymentButtonsRow
+import com.billion_dollor_company.easypay.ui.home.components.ReceiveMoneySection
+import com.billion_dollor_company.easypay.ui.home.components.RecentSection
+import com.billion_dollor_company.easypay.ui.home.components.UserNameSection
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun HomeScreen() {
+
+    val viewModel: HomeViewModel = hiltViewModel()
+
     CommonScaffold(
         hasFAB = true,
         iconFAB = Icons.Default.QrCodeScanner,
     ) {
         CompanyLogo()
-        Spacer(
-            modifier = Modifier
-                .height(16.dp)
-        )
+        HeightSpacer()
+
         UserNameSection()
+        HeightSpacer()
+
+        CreditCard(
+            name = "Anand Bhalerao",
+            cardNumber = "2342543754367564",
+            companyLogo = R.drawable.visa_svg,
+            expiryDate = "09/24"
+        )
+        HeightSpacer()
+
+        PaymentButtonsRow()
+        HeightSpacer()
+
+
+//        UPILiteSection()
+//        HeightSpacer()
+
+        RecentSection(
+            list = viewModel.dummyRecentList
+        )
+        HeightSpacer()
+
+        ReceiveMoneySection()
+        HeightSpacer()
+
+
+        MoreSection()
+        HeightSpacer()
+
     }
 }
 
-@Composable
-fun CompanyLogo() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            imageVector = Icons.Default.PriceChange,
-            contentDescription = "Money Logo",
-            tint = MaterialTheme.colorScheme.primary,
-            modifier = Modifier
-                .size(36.dp)
-        )
-        Text(
-            text = "EasyPay",
-            style = MaterialTheme.typography.displaySmall.copy(
-                fontWeight = FontWeight.ExtraBold
-            ),
-            modifier = Modifier
-                .padding(start = 12.dp)
-        )
-    }
-}
-
-@Composable
-fun UserNameSection() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth(),
-    ) {
-        Column(
-            modifier = Modifier
-                .align(alignment = Alignment.CenterStart)
-        ) {
-            Text(
-                text = "Hello Anand!",
-                style = MaterialTheme.typography.titleLarge.copy(
-                    fontWeight = FontWeight.Bold
-                )
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = "We were waiting for you",
-                style = MaterialTheme.typography.titleMedium.copy(
-                    fontWeight = FontWeight.Thin
-                )
-            )
-        }
-        Card(
-            modifier = Modifier
-                .wrapContentHeight()
-                .wrapContentWidth()
-                .padding(1.dp)
-                .align(alignment = Alignment.CenterEnd),
-            shape = CircleShape,
-            border = BorderStroke(1.5.dp, Color.DarkGray)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.sample_dp),
-                contentDescription = "Profile Picture",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(48.dp)
-                    .clip(CircleShape),
-            )
-        }
-    }
-}
