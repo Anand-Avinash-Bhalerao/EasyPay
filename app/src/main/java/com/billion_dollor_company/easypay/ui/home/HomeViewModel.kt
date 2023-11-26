ee0f791b702f4806aca6514f7d7d22652a50a9e1
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2023-11-26 23:13:18 +0530

    dummy list added

--- app/src/main/java/com/billion_dollor_company/easypay/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/billion_dollor_company/easypay/ui/home/HomeViewModel.kt
@@ -3,11 +3,46 @@ package com.billion_dollor_company.easypay.ui.home
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
+import com.billion_dollor_company.easypay.R
+import com.billion_dollor_company.easypay.models.RecentInteractionInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 @RequiresApi(Build.VERSION_CODES.O)
 class HomeViewModel @Inject constructor() : ViewModel() {
-
+    val dummyRecentList = listOf(
+        RecentInteractionInfo(
+            firstName = "Baburao",
+            userImage = R.drawable.male1
+        ),
+        RecentInteractionInfo(
+            firstName = "Shruti",
+            userImage = R.drawable.female1
+        ),
+        RecentInteractionInfo(
+            firstName = "Raju",
+            userImage = R.drawable.male2
+        ),
+        RecentInteractionInfo(
+            firstName = "Sanya",
+            userImage = R.drawable.female2
+        ),
+        RecentInteractionInfo(
+            firstName = "Rucha",
+            userImage = R.drawable.female4
+        ),
+        RecentInteractionInfo(
+            firstName = "Shyam",
+            userImage = R.drawable.male3
+        ),
+        RecentInteractionInfo(
+            firstName = "Aisha",
+            userImage = R.drawable.female3
+        ),
+        RecentInteractionInfo(
+            firstName = "Abhishek",
+            userImage = R.drawable.male4
+        ),
+    )
 }
