0de658bb446232dc05286ed1a15be1b43720545a
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2023-11-28 00:14:33 +0530

    Arguments added for the amount enter screen

--- app/src/main/java/com/billion_dollor_company/easypay/util/Navigation.kt
+++ app/src/main/java/com/billion_dollor_company/easypay/util/Navigation.kt
@@ -1,23 +1,69 @@
 package com.billion_dollor_company.easypay.util
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.billion_dollor_company.easypay.models.PayeeInfo
+import com.billion_dollor_company.easypay.ui.amount.AmountEnterScreen
 import com.billion_dollor_company.easypay.ui.home.HomeScreen
+import com.billion_dollor_company.easypay.ui.scan.ScanScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route
+        startDestination = Screen.HomeScreen.route
     ) {
         composable(
-            route = Screen.Home.route
+            route = Screen.HomeScreen.route
         ) {
-            HomeScreen()
+            HomeScreen(
+                goScanQR = {
+                    navController.navigate(Screen.ScanScreen.route)
+                }
+            )
+        }
+
+        composable(
+            route = Screen.ScanScreen.route
+        ) {
+            ScanScreen(
+                onScanned = { payeeInfo: PayeeInfo ->
+                    navController.navigate(Screen.AmountEnterScreen.route + "/${payeeInfo.name}/${payeeInfo.phoneNo}/${payeeInfo.upiID}")
+                },
+                onCanceled = {
+                    navController.navigateUp()
+                }
+            )
+        }
+
+        composable(
+            route = Screen.AmountEnterScreen.route + "/{${Constants.NAME}}/{${Constants.PHONE_NO}}/{${Constants.UPI_ID}}",
+            arguments = listOf(
+                navArgument(Constants.NAME) {
+                    type = NavType.StringType
+                },
+                navArgument(Constants.PHONE_NO) {
+                    type = NavType.StringType
+                },
+                navArgument(Constants.UPI_ID) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            AmountEnterScreen(
+                onBackClick = {
+                    navController.navigate(route = Screen.HomeScreen.route)
+                }
+            )
         }
     }
 }
