46d1c617880a264507f6b46ad8ccf83f7eed6d76
Anand Avinash Bhalerao <anandbhalerao.l.a3b@gmail.com>
2023-11-28 00:13:43 +0530

    upiID field added

---
+++ app/src/main/java/com/billion_dollor_company/easypay/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.billion_dollor_company.easypay.util
+
+object Constants {
+
+    const val TAG = "debugging_tag"
+
+    const val NAME = "name"
+    const val PHONE_NO = "phoneNo"
+    const val UPI_ID = "upi_id"
+    object Scan {
+    }
+}
