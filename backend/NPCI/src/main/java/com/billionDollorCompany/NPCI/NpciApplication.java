771f7a1845a867f3d93b4420eaf7b58959f5cdfc
anandbhalerao <anandbhalerao.l.a3b@gmail.com>
2026-05-20 18:46:53 +0530

    Added NPCI Project

---
+++ backend/NPCI/src/main/java/com/billionDollorCompany/NPCI/NpciApplication.java
@@ -0,0 +1,13 @@
+package com.billionDollorCompany.NPCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpciApplication.class, args);
+	}
+
+}
